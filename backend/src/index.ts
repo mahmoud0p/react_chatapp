143824953e57cef00f34007de32b33ca7f4d17ae
mahmoud0p <samir0ps404@example.com>
2025-08-11 16:16:58 +0300

    first commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,13 +18,13 @@ app.use(bodyParser.json());
 app.use('/uploads' ,express.static(path.join(process.cwd(),'/uploads')));
 app.use(cors({
 
-  origin:'http://localhost:5173' ,
+  origin:['http://localhost:5173' , 'http://localhost:5174'] ,
   optionsSuccessStatus: 200 , 
   credentials: true
 }));
 const server = http.createServer(app)
 export const io = new Server(server , { cors:{
-  origin:'http://localhost:5173' ,
+  origin:['http://localhost:5173' , 'http://localhost:5174'] ,
   optionsSuccessStatus: 200 , 
   credentials: true
 } });
